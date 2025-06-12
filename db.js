faf7c76661a0ef22df489ed7b0c929fccab358ed
andriy-dom <domalchukandriy@gmail.com>
2025-06-12 12:37:49 +0200

    initial project structure

---
+++ db.js
@@ -0,0 +1,16 @@
+import mysql from 'mysql2/promise';
+import keys from '.config/keys';
+
+async function createConnection() {
+    const connection = await mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: keys.password,
+        database: 'shop_online'
+    });
+    return connection;
+}
+
+const connection = await createConnection();
+export default connection;
+
