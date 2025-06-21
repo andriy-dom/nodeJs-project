5b1639536e98d154111d497203b63d2daafee171
andriy-dom <domalchukandriy@gmail.com>
2025-06-21 16:01:35 +0200

    add redis and cache

--- controllers/category.js
+++ controllers/category.js
@@ -1,4 +1,5 @@
     import db from '../db.js';
+    import client from '../redisClient.js';
     import errorHandler from '../utils/errorHandler.js';
 
     const createCategory = async (req, res) => {
@@ -14,10 +15,19 @@
     }
 
     const getAllCategories = async (req, res) => {
-        const baseUrl = 'http://localhost:3000'
+        const baseUrl = 'http://localhost:3000';
+        const cacheKey = 'all:categories';
         try {
+            const cached = await client.get(cacheKey);
+            if(cached) {
+                return res.status(200).send(JSON.parse(cached));
+            }
+
             const [categoriesFromDb] = await db.query(`SELECT id, name FROM type_products`);
-            res.status(200).send(categoriesFromDb.map(category => ({id: category.id, name: category.name, url: baseUrl + `/category/products/${category.id}`})))
+            
+            const result = categoriesFromDb.map(category => ({id: category.id, name: category.name, url: baseUrl + `/category/products/${category.id}`}))
+            await client.setEx(cacheKey, 3600, JSON.stringify(result));
+            res.status(200).send(result)
         } catch (err) {
             errorHandler(res, err);
         }
@@ -26,14 +36,24 @@
     const getCategoryItemsById = async (req, res) => {
         const id  = req.params.categoryId;
         const baseUrl = 'http://localhost:3000';
+        const cacheKey = 'all:items:category';
         try {
+            const cached = await client.get(cacheKey);
+            if(cached) {
+                return res.status(200).send(JSON.parse(cached));
+            }
+
             const [rows] = await db.query(`SELECT * FROM products WHERE type_product = ?`, [id]);
             if(rows.length === 0) {
                 res.json({
                     message: 'No products found in this category'
                 })
             }
-            res.status(200).json(rows.map(product => ({product, update: baseUrl + `/product/update/${product.id}`})))
+
+            const result = rows.map(product => ({product, update: baseUrl + `/product/update/${product.id}`}));
+            await client.setEx(cacheKey, 3600, JSON.stringify(result));
+            
+            res.status(200).json(result);
         } catch (err) {
             errorHandler(res, err);
         }
---
+++ redisClient.js
@@ -0,0 +1,8 @@
+import redis from 'redis';
+const client = redis.createClient();
+
+client.connect()
+  .then(() => console.log('Redis connected'))
+  .catch(console.error);
+
+export default client;
